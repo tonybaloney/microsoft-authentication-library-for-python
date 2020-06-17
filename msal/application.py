7f61a420abc65afe4d1fd1a11d3161e008a33092
Ray Luo <rayluo@microsoft.com>
2020-06-16 22:19:02 -0700

    Proof-of-Concept: Removing hardcoded offline_access

--- msal/application.py
+++ msal/application.py
@@ -27,7 +27,7 @@ logger = logging.getLogger(__name__)
 
 def decorate_scope(
         scopes, client_id,
-        reserved_scope=frozenset(['openid', 'profile', 'offline_access'])):
+        reserved_scope=frozenset(['openid', 'profile'])):
     if not isinstance(scopes, (list, set, tuple)):
         raise ValueError("The input scopes should be a list, tuple, or set")
     scope_set = set(scopes)  # Input scopes is typically a list. Copy it to a set.
